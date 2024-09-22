a79461a76c8df61a515c74ed11a519cd92651e01
MHC2005 <mhcedres@gmail.com>
2024-09-22 20:31:52 -0300

    cambios

---
+++ DWM_UT4_TA6-master/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ListaTareas from './components/listaTareas'
+
+function App() {
+
+  return (
+    <>
+      <ListaTareas />
+    </>
+  )
+}
+
+export default App
